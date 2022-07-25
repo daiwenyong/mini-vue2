381612bb8bdd0c2ea65be3e2d5eca6c3f39978c6
daiwenyong <1106085869@qq.com>
2022-07-25 21:23:24 +0800

    feat:mini-vue2

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ export default Vue
 //     <li key="C">C1</li>
 //     <li key="D">D1</li>
 // </div>`;
-// // let vm2 = new Vue({ data: { message: 'zf' } });
+// // let vm2 = new Vue({ data: { message: 'hello' } });
 // const render2 = compileToFunction(newTemplate)
 // const newVnode = render2.call(vm); // 虚拟dom
 // // 根据新的虚拟节点更新老的节点，老的能复用尽量复用
